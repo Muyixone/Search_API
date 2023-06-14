5df812081eb8b7ebc732138e2c263cae1badd8c1
Osamuyi <muyixone@yahoo.com>
2023-06-14 16:46:37 +0100

    Create an asynWrapper to replace the try/catch blocks in the controller

--- controller/products.js
+++ controller/products.js
@@ -1,14 +1,21 @@
-const getAllProducts = (req, res) => {
+const products = require('../models/product');
+const asyncWrapper = require('../middlewares/async-wrapper');
+
+const getAllProducts = asyncWrapper(async (req, res) => {
+  const product = await products.find({});
   res.status(200).json({
     msg: 'Products available',
+    product,
   });
-};
+});
 
-const createProduct = (req, res) => {
+const createProduct = asyncWrapper(async (req, res) => {
+  const product = await products.create(req.body);
   res.status(200).json({
     msg: 'Products created successfully',
+    product,
   });
-};
+});
 
 module.exports = {
   getAllProducts,
