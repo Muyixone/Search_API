aaaeb46d216b8507922a90d2f3d5bf3ab124f7c4
Osamuyi <muyixone@yahoo.com>
2023-06-15 23:48:00 +0100

    Create the pagination logic

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 An API that performs search queries.
 The results can be sorted and filtered
+
+- To filter by either name company, use the "search" key. For example
+  localhost:5000/api/v1/products?search=...
+
+- To filter by price equal to specified value, use the
+  localhost:5000/api/v1/products?price=...
+
+- To filter by price less than specified value, use the
+  localhost:5000/api/v1/products?price=...&operator=<
+
+- To filter by price greater than specified value, use the
+  localhost:5000/api/v1/products?price=...&operator=>
+
+- To filter by price between range of specified values, use the
+  localhost:5000/api/v1/products?price=baseValue.&operator=range&minValue=...&maxValue=...
+  Note that baseValue should be same as minValue
--- controller/products.js
+++ controller/products.js
@@ -4,20 +4,68 @@ const asyncWrapper = require('../middlewares/async-wrapper');
 const getAllProducts = asyncWrapper(async (req, res) => {
   let match = {};
 
-  if (req.query.name) {
-    match.name = req.query.name;
+  let page = req.query.page || 1;
+  let docsPerPage = 2;
+  let skip = docsPerPage * (page - 1);
+  let limit = docsPerPage;
+
+  // FILTER QUERIES
+  // filter by name or company
+  if (req.query.search) {
+    match.$or = [
+      { name: new RegExp(req.query.search, 'i') },
+      { company: new RegExp(req.query.search, 'i') },
+    ];
   }
+  // filter by rating
+  if (req.query.rating) {
+    match.rating = +req.query.rating;
+  }
+
+  // filter by price
+  if (req.query.price) {
+    const priceToNumber = parseInt(req.query.price);
+    const operator = req.query.operator;
+
+    switch (operator) {
+      case '<':
+        match.price = { $lt: priceToNumber };
+        break;
+      case '>':
+        match.price = { $gt: priceToNumber };
+        break;
+      case 'range':
+        const minValue = parseInt(req.query.minValue);
+        const maxValue = parseInt(req.query.maxValue);
+        match.price = { $lte: maxValue, $gte: minValue };
+        break;
+      default:
+        match.price = priceToNumber;
+    }
+  }
+
+  // filter by featured
   if (req.query.featured) {
-    match.featured = req.query.featured;
+    match.featured = req.query.featured === 'true' ? true : false;
   }
-  let product = await products.aggregate([{ $match: match }]);
+
+  // PAGINATION
+  let facet = {};
+  facet.data = [{ $skip: skip }, { $limit: limit }];
+
+  let product = await products.aggregate([
+    { $match: match },
+    { $facet: facet },
+    {
+      $project: {
+        docs: '$data',
+      },
+    },
+  ]);
 
   res.status(200).json({
-    nbHits: product.length,
-    data: {
-      msg: 'Products available',
-      product,
-    },
+    msg: 'Products available',
+    product,
   });
 });
 
